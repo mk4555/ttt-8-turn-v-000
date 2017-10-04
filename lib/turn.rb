80f72cce7def1a0bad0a9d1bff0a77d238f73eec
Min Kim <mk4555@nyu.edu>
2017-10-04 14:07:24 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,9 +19,9 @@ def valid_move?(board, index)
 end
 
 def position_taken?(board, index)
-  if board[index] != " " || board[index] != "" || board[index] != nil
-    true
-  else
+  if board[index] == " " || board[index] == "" || board[index] == nil
     false
+  else
+    true
   end
 end
