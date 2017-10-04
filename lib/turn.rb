ec33fc92cdf7aa66efad3f6c799560f9e5d3aa88
Min Kim <mk4555@nyu.edu>
2017-10-04 14:42:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,4 +39,9 @@ def turn(board)
   puts "Please enter 1-9:"
   index = gets.chomp
   index = input_to_index(index)
+  if valid_move?(board, index)
+    move(board, index)
+  else
+    turn(board)
+  end
 end
